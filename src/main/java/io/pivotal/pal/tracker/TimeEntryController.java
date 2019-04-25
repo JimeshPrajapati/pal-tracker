50f4799748c02bbb8faabd3310ee24da83a3aa01
Prajapati Jimesh Rakeshbh - (Digital) <PR20077157@wipro.com>
2019-04-25 15:00:49 +0530

    MVC Commit

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -6,18 +6,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/time-entries")
 public class TimeEntryController {
 
     //@Autowired
     TimeEntryRepository timeRepo;
 
-    @GetMapping(value="/get")
+    @GetMapping
     public ResponseEntity<List<TimeEntry>> list()
     {
         return new ResponseEntity<>(timeRepo.list(),HttpStatus.OK);
     }
 
-    @GetMapping (value="/get/{Id}")
+    @GetMapping("{Id}")
     public ResponseEntity<TimeEntry> read(@PathVariable Long Id)
     {
         TimeEntry timeEntry = timeRepo.find(Id);
@@ -33,14 +35,14 @@ public class TimeEntryController {
         this.timeRepo=timeRepo;
     }
 
-    @PostMapping (value="/create")
+    @PostMapping
     public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntry)
     {
         TimeEntry newTimeEntry= timeRepo.create(timeEntry);
         return new ResponseEntity<>(newTimeEntry, HttpStatus.CREATED);
     }
 
-    @PutMapping(value="/put/{Id}")
+    @PutMapping("{Id}")
     public ResponseEntity update(@PathVariable Long Id,@RequestBody TimeEntry timeEntry)
     {
         TimeEntry updatedTimeEntry = timeRepo.update(Id,timeEntry);
@@ -51,7 +53,7 @@ public class TimeEntryController {
         return new ResponseEntity<>(updatedTimeEntry, HttpStatus.OK);
     }
 
-    @DeleteMapping(value="/delete/{Id}")
+    @DeleteMapping("{Id}")
     public ResponseEntity<TimeEntry> delete(@PathVariable Long Id)
     {
         timeRepo.delete(Id);
